8229a7b8d2baf03ec1b0e2c2f780cc35a82b6782
gaandesign <marinagarridoandres@gmail.com>
2024-12-25 20:47:26 +0100

    inicio+menu+introduccion+video+cards

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// console.log("online");
+
+// LIBRERIA AOS
+window.onload = function () {
+    AOS.init({
+        duration: 1200,
+        // once: true,
+    });
+};
+
+
+$(document).ready(function(){
+
+    // POP UP
+
+    // Muestra el pop-up de cookies después de 2 segundos
+    setTimeout(function(){
+      $("#cookie-popup").fadeIn();
+    }, 2000);
+
+    // Maneja el clic en el botón de aceptar cookies
+    $("#accept-cookies").click(function(){
+        $("#cookie-popup").fadeOut();
+    });   
+
+    // Maneja el clic en el botón de rechazar cookies
+    $("#reject-cookies").click(function() {
+        $("#cookie-popup").fadeOut();
+    });
+
+});
+
